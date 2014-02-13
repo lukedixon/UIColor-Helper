45cb39d8af586d602781c23bc4420c52b4da27b7
luke.dixon <luke.dixon@57digital.co.uk>
2014-02-13 11:55:53 +0000

    Added UIColor+Helper

---
+++ UIColor+Helper.h
@@ -0,0 +1,14 @@
+//
+//  UIColor+Helper.h
+//
+//  Created by Luke Dixon on 13/02/2014.
+//  Copyright (c) 2014 Private. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Helper)
+
++ (UIColor *)defaultColor;
+
+@end
---
+++ UIColor+Helper.m
@@ -0,0 +1,16 @@
+//
+//  UIColor+Helper.m
+//
+//  Created by Luke Dixon on 13/02/2014.
+//  Copyright (c) 2014 Private. All rights reserved.
+//
+
+#import "UIColor+Helper.h"
+
+@implementation UIColor (Helper)
+
++ (UIColor *)defaultColor {
+    return [UIColor blueColor];
+}
+
+@end
